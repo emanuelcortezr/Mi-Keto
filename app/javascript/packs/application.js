cf0d20a6ad230fe818c19ce70a37a4e3d64f27f2
Manuel Cortez <emanuelcorteszr@gmail.com>
2020-02-15 17:49:30 -0300

    readme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//=Require jquery3
+
+//=Require popper
+
+//=Require bootstrap
